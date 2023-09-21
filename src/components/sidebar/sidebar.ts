396f87ba9fac8429cd98ddfbe4451f1ebd5b0b73
LeJuanTenorio <tenorio2003@gmail.com>
2023-09-21 11:48:23 -0500

    org[sidebar]

---
+++ src/components/sidebar/sidebar.ts
@@ -0,0 +1,28 @@
+class sidebar extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
+        render(){
+            if(this.shadowRoot){
+                this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="/src/components/sidebar/sidebar.css">
+                <div class="sidebar">
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">search</a></li>
+                    <li><a href="#">notifications</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+                `
+            }
+        }
+    }
+customElements.define("my-sidebar",sidebar);
+export default sidebar;
