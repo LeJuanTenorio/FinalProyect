2c0b97ec8da94e8e22f189ee2d93e6af1aa7b326
LeJuanTenorio <tenorio2003@gmail.com>
2023-09-22 10:50:58 -0500

    fix

--- src/components/user/user.ts
+++ src/components/user/user.ts
@@ -1,47 +1,47 @@
 export enum Attribute {
-  "id" = "id",
-  "name" = "name",
-  "img" = "img",
-}
-
-class User extends HTMLElement {
-  name?: string;
-  img?: string;
-
-  static get observedAttributes() {
-    const attrs: Record<Attribute, null> = {
-      id: null,
-      name: null,
-      img: null,
-    };
-    return Object.keys(attrs);
+    id = "id",
+    name = "name",
+    img = "img",
   }
-
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
-
-  connectedCallback() {
-    this.name = this.name;
-    this.img = this.img;
-
-    this.render();
-  }
-
-  render() {
-    if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = `
-        <h1>${this.name}</h1>
-        <img src="${this.img}"></img>
-        <style>
-        </style>
+  
+  class User extends HTMLElement {
+    name?: string;
+    img?: string;
+  
+    static get observedAttributes() {
+      return [Attribute.id, Attribute.name, Attribute.img];
+    }
+  
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+    }
+  
+    connectedCallback() {
+      this.name = this.getAttribute(Attribute.name) || "";
+      this.img = this.getAttribute(Attribute.img) || "";
+  
+      this.render();
+    }
+  
+    render() {
+      if (this.shadowRoot) {
+        this.shadowRoot.innerHTML = `
+          <h1>${this.name}</h1>
+          <img src="${this.img}">
+          <style>
+          </style>
         `;
-
-      
+      }
     }
   }
-}
-
-customElements.define("user-info", User);
-export default User;
+  
+  customElements.define("user-info", User);
+  export default User;
+ 
+  
+  
+  
+  
+  
+  
