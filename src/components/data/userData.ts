db65434826d81a195091a006398ab32e8666bd59
LeJuanTenorio <tenorio2003@gmail.com>
2023-09-22 11:54:50 -0500

    add[stuff]

--- src/components/data/userData.ts
+++ src/components/data/userData.ts
@@ -2,6 +2,6 @@ export const userData = [
     {
         "name": "Julian",
         "id": "4324235f",
-        "img": "https://images.unsplash.com/photo-1623184663110-89ba5b565eb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c21pbGluZyUyMG1hbnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+        "img": "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
     },
 ];
