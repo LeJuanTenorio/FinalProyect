92a3c54df23b8346c4a975a641e43d8160338a4b
LeJuanTenorio <tenorio2003@gmail.com>
2023-09-22 15:37:17 -0500

    fix[poster]

--- src/components/seriesPoster/poster.ts
+++ src/components/seriesPoster/poster.ts
@@ -1,42 +1,40 @@
-import style from "./poster.css"
+import style from "./poster.css";
 
 export enum Attribute {
-    poster = "poster"
+  poster = "poster",
+}
+
+class Poster extends HTMLElement {
+  poster?: string;
+
+  static get observedAttributes() {
+    return [Attribute.poster];
   }
-  
-  class Poster extends HTMLElement {
-    poster?: string;
-  
-    static get observedAttributes() {
-      return [Attribute.poster];
-    }
-  
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-    }
-  
-    connectedCallback() {
-      this.poster = this.getAttribute(Attribute.poster) || "";
-  
-      this.render();
-    }
-  
-    render() {
-      if (this.shadowRoot) {
-        this.shadowRoot.innerHTML = `
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.poster = this.getAttribute(Attribute.poster) || "";
+
+    this.render();
+  }
+
+  render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
           <img class="img" src="${this.poster}" onerror="this.style.display='none';">
           <style>
           ${style}
           </style>
         `;
-      }
     }
   }
-  
+}
+
+if (!customElements.get("poster-img")) {
   customElements.define("poster-img", Poster);
-  export default Poster;
- 
-  
-  
-  
+}
+export default Poster;
