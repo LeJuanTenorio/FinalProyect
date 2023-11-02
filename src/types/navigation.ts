694848b0a51eceb74250d1f564e964d0ac10559e
LeJuanTenorio <tenorio2003@gmail.com>
2023-11-02 15:05:48 -0500

    add[updatedNavScreens]

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,5 +1,10 @@
 export enum Screens{
     LOGIN = "LOGIN",
     DASHBOARD = "DASHBOARD",
-    SIGNUP = "SIGNUP"
+    SIGNUP = "SIGNUP",
+    SEARCH = "SEARCH",
+    SERIES = "SERIES",
+    LANDING = "LANDING",
+    PROFILE = "PROFILE",
+    CONFIG = "CONFIG"
 }
