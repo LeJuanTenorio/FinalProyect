bb5cf3486661738d92b4358fbf4ccf13c4ed8e95
LeJuanTenorio <tenorio2003@gmail.com>
2023-11-25 16:30:45 -0500

    solving problems

--- src/screens/login/login.ts
+++ src/screens/login/login.ts
@@ -29,55 +29,115 @@ class Login extends HTMLElement {
     }
 
     render() {
-        if(this.shadowRoot){
-            this.shadowRoot.innerHTML = `
-            <style>
-            ${styles}
-            </style>
+        if (this.shadowRoot) {
+            const container = this.ownerDocument.createElement('div');
     
-            <body>
-            <img class ="background" src="https://www.themoviedb.org/t/p/original/cCVyNQkFZxLAC5jKXgO7EsJlDNd.jpg">
-    <section>
+            const styleTag = this.ownerDocument.createElement('style');
+            styleTag.textContent = styles;
+            container.appendChild(styleTag);
+    
+            const section = this.ownerDocument.createElement('section');
+    
+            const formularioInicioDiv = this.ownerDocument.createElement('div');
+            formularioInicioDiv.classList.add('formulario-inicio');
+    
+            const tituloP = this.ownerDocument.createElement('p');
+            tituloP.classList.add('titulo');
+            tituloP.textContent = 'INICIA SESIÓN';
+            formularioInicioDiv.appendChild(tituloP);
+    
+            const formularioLogin = this.ownerDocument.createElement('form');
+            formularioLogin.id = 'formulario-login';
+    
+            const emailInput = this.ownerDocument.createElement('input');
+            emailInput.classList.add('form-control', 'email');
+            emailInput.type = 'email';
+            emailInput.id = 'correo';
+            emailInput.placeholder = 'Correo electrónico';
+            formularioLogin.appendChild(emailInput);
+            emailInput.addEventListener("change", this.changeEmail);
+    
+            const passwordInput = this.ownerDocument.createElement('input');
+            passwordInput.classList.add('form-control', 'password');
+            passwordInput.type = 'password';
+            passwordInput.id = 'contrasena';
+            passwordInput.placeholder = 'Contraseña';
+            passwordInput.addEventListener("change", this.changePassword);
+            formularioLogin.appendChild(passwordInput);
 
-        <div class="formulario-inicio">
-            <p class="titulo">INICIA SESIÓN</p> 
-            <form id="formulario-login">
-                <input class="form-control" class="email" type="email" id="correo"  placeholder="Correo electrónico" />
-                <input class="form-control" class="password" type="password" id="contrasena"  placeholder= "Contraseña" />
-                
-                <form action="./perfiles-.html">
-                <input type="submit" class="loginBtn" id="loginBtn" value="Iniciar Sesión">
-                </form>
-            </form>
-        
-            <div class="recuerdame">
-                <div id="boton-check">
-                    <input type="checkbox" id="btn-switch">
-                    <label for="btn-switch" class="lbl-switch"></label>
-                </div>
-                <label class="label">Recuerdame</label>
-                <a class="ayuda" href="#">¿Necesitas Ayuda?</a>
-            </div>
-           
-            <div class="registrate">
-                <label class="labelp">¿Primera vez en SPHERE?</label>
-                <div class="boton-registro">
-                    <button id="boton-registro" class="botonRegistro" href = "./registro.html" class="form-control">Registrate</button>
-                </div>
-            </div>
-            
-        </div>
-    </section>
-</body>`;
-            const email = this.shadowRoot.querySelector(".email")
-            email?.addEventListener("change", this.changeEmail);
+    
+            const formAction = this.ownerDocument.createElement('form');
+            formAction.action = './perfiles-.html';
+    
+            const loginBtnInput = this.ownerDocument.createElement('input');
+            loginBtnInput.type = 'submit';
+            loginBtnInput.classList.add('loginBtn');
+            loginBtnInput.id = 'loginBtn';
+            loginBtnInput.value = 'Iniciar Sesión';
+            loginBtnInput.addEventListener("click", this.submitLogin);
+            formAction.appendChild(loginBtnInput);
+    
+            formularioLogin.appendChild(formAction);
+    
+            formularioInicioDiv.appendChild(formularioLogin);
+    
+            const recuerdameDiv = this.ownerDocument.createElement('div');
+            recuerdameDiv.classList.add('recuerdame');
+    
+            const botonCheckDiv = this.ownerDocument.createElement('div');
+    
+            const switchInput = this.ownerDocument.createElement('input');
+            switchInput.type = 'checkbox';
+            switchInput.id = 'btn-switch';
+            botonCheckDiv.appendChild(switchInput);
+    
+            const labelSwitch = this.ownerDocument.createElement('label');
+            labelSwitch.classList.add('lbl-switch');
+            botonCheckDiv.appendChild(labelSwitch);
+    
+            recuerdameDiv.appendChild(botonCheckDiv);
+    
+            const labelRecuerdame = this.ownerDocument.createElement('label');
+            labelRecuerdame.classList.add('label');
+            labelRecuerdame.textContent = 'Recuerdame';
+            recuerdameDiv.appendChild(labelRecuerdame);
+    
+            const ayudaA = this.ownerDocument.createElement('a');
+            ayudaA.classList.add('ayuda');
+            ayudaA.href = '#';
+            ayudaA.textContent = '¿Necesitas Ayuda?';
+            recuerdameDiv.appendChild(ayudaA);
+    
+            formularioInicioDiv.appendChild(recuerdameDiv);
+    
+            const registrateDiv = this.ownerDocument.createElement('div');
+            registrateDiv.classList.add('registrate');
+    
+            const labelpRegistrate = this.ownerDocument.createElement('label');
+            labelpRegistrate.classList.add('labelp');
+            labelpRegistrate.textContent = '¿Primera vez en SPHERE?';
+            registrateDiv.appendChild(labelpRegistrate);
+    
+            const botonRegistroDiv = this.ownerDocument.createElement('div');
+    
+            const botonRegistroBtn = this.ownerDocument.createElement('button');
+            botonRegistroBtn.id = 'boton-registro';
+            botonRegistroBtn.classList.add('botonRegistro', 'form-control');
+            botonRegistroBtn.textContent = 'Registrate';
+    
+            botonRegistroDiv.appendChild(botonRegistroBtn);
+    
+            registrateDiv.appendChild(botonRegistroDiv);
+    
+            formularioInicioDiv.appendChild(registrateDiv);
+    
+            section.appendChild(formularioInicioDiv);
+            container.appendChild(section);
+    
+            this.shadowRoot.appendChild(container);
 
-            const password = this.shadowRoot.querySelector(".password")
-            password?.addEventListener("change", this.changePassword);
-
-            const loginBtn = this.shadowRoot.querySelector(".loginBtn");
-            loginBtn?.addEventListener("click", this.submitLogin);
-        }}
+        }
+    }
     }
         
 
--- src/screens/signUp/signUp.ts
+++ src/screens/signUp/signUp.ts
@@ -48,123 +48,30 @@ class SignUp extends HTMLElement {
     }
 
     render() {
-        if (this.shadowRoot) {
+        if(this.shadowRoot){
+            const container = this.ownerDocument.createElement('div')
 
-            this.shadowRoot.innerHTML = `
-            <style>
-            ${styles}
-            </style>`
+            const email = this.ownerDocument.createElement('input')
+            email?.addEventListener("change", this.changeEmail);
+            email.placeholder = "email"
+            container.appendChild(email)
 
-            const container = this.ownerDocument.createElement('div');
-    
-            const backgroundImg = this.ownerDocument.createElement('img');
-            backgroundImg.classList.add('background');
-            backgroundImg.src = 'https://www.themoviedb.org/t/p/original/wX3twEPd6RCkQly9JVEkw4a6Qbi.jpg';
-            container.appendChild(backgroundImg);
-    
-            const section = this.ownerDocument.createElement('section');
-    
-            const logoDiv = this.ownerDocument.createElement('div');
-            logoDiv.classList.add('logo');
-    
-            const logoImg = this.ownerDocument.createElement('img');
-            logoImg.classList.add('sphere');
-            logoImg.src = 'https://raw.githubusercontent.com/Manuelard03/GHIFLIX/main/3.%20REGISTRO/img/1-LOGO.png';
-            logoDiv.appendChild(logoImg);
-    
-            section.appendChild(logoDiv);
-    
-            const cuadroDiv = this.ownerDocument.createElement('div');
-            cuadroDiv.classList.add('cuadro');
-    
-            const tituloP = this.ownerDocument.createElement('p');
-            tituloP.classList.add('titulo');
-            tituloP.textContent = 'REGISTRATE';
-            cuadroDiv.appendChild(tituloP);
-    
-            const formularioRegistro = this.ownerDocument.createElement('form');
-            formularioRegistro.id = 'formulario-registro';
-    
-            const usernameInput = this.ownerDocument.createElement('input');
-            usernameInput.classList.add('form-control', 'username');
-            usernameInput.type = 'text';
-            usernameInput.id = 'nombre';
-            usernameInput.required = true;
-            usernameInput.placeholder = 'Username';
-            formularioRegistro.appendChild(usernameInput);
-    
-            const phoneInput = this.ownerDocument.createElement('input');
-            phoneInput.classList.add('form-control', 'phone');
-            phoneInput.type = 'tel';
-            phoneInput.id = 'telefono';
-            phoneInput.placeholder = 'Telefono';
-            formularioRegistro.appendChild(phoneInput);
-    
-            const emailInput = this.ownerDocument.createElement('input');
-            emailInput.classList.add('form-control', 'email');
-            emailInput.type = 'email';
-            emailInput.id = 'correo';
-            emailInput.required = true;
-            emailInput.placeholder = 'Correo';
-            formularioRegistro.appendChild(emailInput);
-    
-            const passwordInput = this.ownerDocument.createElement('input');
-            passwordInput.classList.add('form-control', 'password');
-            passwordInput.type = 'password';
-            passwordInput.id = 'contrasena';
-            passwordInput.required = true;
-            passwordInput.placeholder = 'Contraseña';
-            formularioRegistro.appendChild(passwordInput);
-    
-            const registerInput = this.ownerDocument.createElement('input');
-            registerInput.classList.add('form-control', 'register');
-            registerInput.type = 'submit';
-            registerInput.id = 'registrarse';
-            registerInput.value = 'Registrarme';
-            formularioRegistro.appendChild(registerInput);
-    
-            cuadroDiv.appendChild(formularioRegistro);
-    
-            const listaDiv = this.ownerDocument.createElement('div');
-            listaDiv.id = 'lista';
-            cuadroDiv.appendChild(listaDiv);
-    
-            const terminosDiv = this.ownerDocument.createElement('div');
-            terminosDiv.classList.add('terminos');
-    
-            const botonTerminosDiv = this.ownerDocument.createElement('div');
-    
-            const switchInput = this.ownerDocument.createElement('input');
-            switchInput.type = 'checkbox';
-            switchInput.id = 'btn-switch';
-            switchInput.required = true;
-            botonTerminosDiv.appendChild(switchInput);
-    
-            const labelSwitch = this.ownerDocument.createElement('label');
-            labelSwitch.classList.add('lbl-switch');
-            botonTerminosDiv.appendChild(labelSwitch);
-    
-            terminosDiv.appendChild(botonTerminosDiv);
-    
-            cuadroDiv.appendChild(terminosDiv);
-    
-            const inicioSesionDiv = this.ownerDocument.createElement('div');
-            inicioSesionDiv.classList.add('inicia-sesion');
-    
-            const labelInicioSesion = this.ownerDocument.createElement('label');
-            labelInicioSesion.classList.add('label');
-            labelInicioSesion.innerHTML = '¿Ya tienes una cuenta? <a href="/iniciar-sesion.html">Inicia Sesión</a>';
-            inicioSesionDiv.appendChild(labelInicioSesion);
-    
-            cuadroDiv.appendChild(inicioSesionDiv);
-    
-            section.appendChild(cuadroDiv);
-            container.appendChild(section);
-    
+            const password = this.ownerDocument.createElement('input')
+            password?.addEventListener("change", this.changePassword);
+            password.placeholder = "password"
+            container.appendChild(password)
+
+            const name = this.ownerDocument.createElement('input')
+            name?.addEventListener("change", this.changeUsername);
+            name.placeholder = "name"
+            container.appendChild(name)
+
+            const loginBtn = this.ownerDocument.createElement('button');
+            loginBtn?.addEventListener("click", this.submitSignUp);
+            container.appendChild(loginBtn)
+
             this.shadowRoot.appendChild(container);
-        }
-    }
-    
+        }}
     }
         
 
