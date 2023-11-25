0e2557343518f4af447afa8d991fd5fe20d4d62a
LeJuanTenorio <tenorio2003@gmail.com>
2023-11-25 15:09:57 -0500

    eureka moment

--- src/screens/signUp/signUp.ts
+++ src/screens/signUp/signUp.ts
@@ -5,7 +5,6 @@ import styles from "./styles.css";
 import firebase from "../../services/firebase";
 
 const formPost = {
-    username:"",
     name:"",
     email: "",
     phone: "",
@@ -36,10 +35,12 @@ class SignUp extends HTMLElement {
 
     changePassword(e:any){
         formPost.password = e.target.value;
+        console.log(e.target.value)
     }
 
     changeUsername(e:any){
-        formPost.username = e.target.value;
+        formPost.name = e.target.value;
+        console.log(e.target.value)
     }
 
     changePhone(e:any){
@@ -47,55 +48,123 @@ class SignUp extends HTMLElement {
     }
 
     render() {
-        if(this.shadowRoot){
+        if (this.shadowRoot) {
+
             this.shadowRoot.innerHTML = `
             <style>
             ${styles}
-            </style>
+            </style>`
+
+            const container = this.ownerDocument.createElement('div');
     
-            <body>
-            <img class="background"src="https://www.themoviedb.org/t/p/original/wX3twEPd6RCkQly9JVEkw4a6Qbi.jpg">
-    <section>
-        <div class="logo">
-                <img class="sphere"src="https://raw.githubusercontent.com/Manuelard03/GHIFLIX/main/3.%20REGISTRO/img/1-LOGO.png">
-        </div>
-        <div class="cuadro">
-            <p class="titulo"">REGISTRATE</p> 
-
-            <form id="formulario-registro">    
-                <input class="form-control" class="username" type="text" id="nombre" required placeholder= "Username" />
-                <input class="form-control" class="phone" type="tel" id="telefono" placeholder= "Telefono" />
-                <input class="form-control" class="email" type="email" id="correo" required placeholder= "Correo" />
-                <input class="form-control" class="password" type="password" id="contrasena" required placeholder="Contraseña" />
-                <input class="form-control" class="register" type="submit" id="registrarse" value="Registrarme" />
-            </form>
-
-            <div id="lista"></div>
-            
-            <div class="terminos">
-                <div id="boton-terminos">
-                    <input type="checkbox" id="btn-switch" required />
-                    <label for="btn-switch" class="lbl-switch"></label>
-                </div>
-            </div>
-            
-            <div class="inicia-sesion">
-                <label class="label">¿Ya tienes una cuenta? <a href="/iniciar-sesion.html">Inicia Sesión</a></label>
-            </div>
-        </div>
-    </section>
-
+            const backgroundImg = this.ownerDocument.createElement('img');
+            backgroundImg.classList.add('background');
+            backgroundImg.src = 'https://www.themoviedb.org/t/p/original/wX3twEPd6RCkQly9JVEkw4a6Qbi.jpg';
+            container.appendChild(backgroundImg);
     
-</body>`;
-            const email = this.shadowRoot.querySelector(".email")
-            email?.addEventListener("change", this.changeEmail);
-
-            const password = this.shadowRoot.querySelector(".password")
-            password?.addEventListener("change", this.changePassword);
-
-            const loginBtn = this.shadowRoot.querySelector(".register");
-            loginBtn?.addEventListener("click", this.submitSignUp);
-        }}
+            const section = this.ownerDocument.createElement('section');
+    
+            const logoDiv = this.ownerDocument.createElement('div');
+            logoDiv.classList.add('logo');
+    
+            const logoImg = this.ownerDocument.createElement('img');
+            logoImg.classList.add('sphere');
+            logoImg.src = 'https://raw.githubusercontent.com/Manuelard03/GHIFLIX/main/3.%20REGISTRO/img/1-LOGO.png';
+            logoDiv.appendChild(logoImg);
+    
+            section.appendChild(logoDiv);
+    
+            const cuadroDiv = this.ownerDocument.createElement('div');
+            cuadroDiv.classList.add('cuadro');
+    
+            const tituloP = this.ownerDocument.createElement('p');
+            tituloP.classList.add('titulo');
+            tituloP.textContent = 'REGISTRATE';
+            cuadroDiv.appendChild(tituloP);
+    
+            const formularioRegistro = this.ownerDocument.createElement('form');
+            formularioRegistro.id = 'formulario-registro';
+    
+            const usernameInput = this.ownerDocument.createElement('input');
+            usernameInput.classList.add('form-control', 'username');
+            usernameInput.type = 'text';
+            usernameInput.id = 'nombre';
+            usernameInput.required = true;
+            usernameInput.placeholder = 'Username';
+            formularioRegistro.appendChild(usernameInput);
+    
+            const phoneInput = this.ownerDocument.createElement('input');
+            phoneInput.classList.add('form-control', 'phone');
+            phoneInput.type = 'tel';
+            phoneInput.id = 'telefono';
+            phoneInput.placeholder = 'Telefono';
+            formularioRegistro.appendChild(phoneInput);
+    
+            const emailInput = this.ownerDocument.createElement('input');
+            emailInput.classList.add('form-control', 'email');
+            emailInput.type = 'email';
+            emailInput.id = 'correo';
+            emailInput.required = true;
+            emailInput.placeholder = 'Correo';
+            formularioRegistro.appendChild(emailInput);
+    
+            const passwordInput = this.ownerDocument.createElement('input');
+            passwordInput.classList.add('form-control', 'password');
+            passwordInput.type = 'password';
+            passwordInput.id = 'contrasena';
+            passwordInput.required = true;
+            passwordInput.placeholder = 'Contraseña';
+            formularioRegistro.appendChild(passwordInput);
+    
+            const registerInput = this.ownerDocument.createElement('input');
+            registerInput.classList.add('form-control', 'register');
+            registerInput.type = 'submit';
+            registerInput.id = 'registrarse';
+            registerInput.value = 'Registrarme';
+            formularioRegistro.appendChild(registerInput);
+    
+            cuadroDiv.appendChild(formularioRegistro);
+    
+            const listaDiv = this.ownerDocument.createElement('div');
+            listaDiv.id = 'lista';
+            cuadroDiv.appendChild(listaDiv);
+    
+            const terminosDiv = this.ownerDocument.createElement('div');
+            terminosDiv.classList.add('terminos');
+    
+            const botonTerminosDiv = this.ownerDocument.createElement('div');
+    
+            const switchInput = this.ownerDocument.createElement('input');
+            switchInput.type = 'checkbox';
+            switchInput.id = 'btn-switch';
+            switchInput.required = true;
+            botonTerminosDiv.appendChild(switchInput);
+    
+            const labelSwitch = this.ownerDocument.createElement('label');
+            labelSwitch.classList.add('lbl-switch');
+            botonTerminosDiv.appendChild(labelSwitch);
+    
+            terminosDiv.appendChild(botonTerminosDiv);
+    
+            cuadroDiv.appendChild(terminosDiv);
+    
+            const inicioSesionDiv = this.ownerDocument.createElement('div');
+            inicioSesionDiv.classList.add('inicia-sesion');
+    
+            const labelInicioSesion = this.ownerDocument.createElement('label');
+            labelInicioSesion.classList.add('label');
+            labelInicioSesion.innerHTML = '¿Ya tienes una cuenta? <a href="/iniciar-sesion.html">Inicia Sesión</a>';
+            inicioSesionDiv.appendChild(labelInicioSesion);
+    
+            cuadroDiv.appendChild(inicioSesionDiv);
+    
+            section.appendChild(cuadroDiv);
+            container.appendChild(section);
+    
+            this.shadowRoot.appendChild(container);
+        }
+    }
+    
     }
         
 
