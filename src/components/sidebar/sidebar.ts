87235a5ae1f36fab10206ee4fd197d14d2169b3e
LeJuanTenorio <tenorio2003@gmail.com>
2023-12-01 17:23:54 -0500

    add[workingCSSLOGO]

--- src/components/sidebar/sidebar.ts
+++ src/components/sidebar/sidebar.ts
@@ -49,7 +49,7 @@ class Sidebar extends HTMLElement {
                 <li><a class="home"><img src="https://cdn-icons-png.flaticon.com/128/1946/1946436.png"></a></li>                
                 <li><a class="config"><img src="https://cdn-icons-png.flaticon.com/512/15/15185.png"></a></li>
                 <li><a class="profile"><img src="https://cdn-icons-png.flaticon.com/128/1077/1077114.png?ga=GA1.1.432586728.1695342026&track=ais"></a></li>
-                <li><a class="signOut"><img src="https://cdn-icons-png.flaticon.com/128/149/149852.png?ga=GA1.1.432586728.1695342026&track=ais"></a></li>
+                <li><a class="signOut"><img src="https://static-00.iconduck.com/assets.00/log-out-icon-2048x2048-cru8zabe.png"></a></li>
 
                 </ul>
             </div>
